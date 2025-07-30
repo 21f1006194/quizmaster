1bcd2ff236d4b92a8efd467b93524a42589b7d03
jishnujp <jishnujprksh007@gmail.com>
2025-07-30 23:29:59 +0530

    Bugfix - lazy loading on detached objects   created issue in cached objects
    solved by doing joinedload

--- backend/app/services/catalog/question_service.py
+++ backend/app/services/catalog/question_service.py
@@ -1,14 +1,19 @@
 from app.models.question import Question
 from app import cache
+from sqlalchemy.orm import joinedload
 
 
 @cache.memoize(timeout=3600)
 def get_questions_by_quiz(quiz_id):
     print(f"Getting questions by quiz : {quiz_id}")
-    questions = Question.query.filter_by(quiz_id=quiz_id).all()
+    questions = (
+        Question.query.options(joinedload(Question.options))
+        .filter_by(quiz_id=quiz_id)
+        .all()
+    )
     return questions
 
 
 def get_question_by_id(question_id):
-    question = Question.query.get(question_id)
+    question = Question.query.options(joinedload(Question.options)).get(question_id)
     return question
